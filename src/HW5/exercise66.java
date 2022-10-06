05e6453a16bde1ccc77e9ca494271e1dd19313ed
cahine22 <carolinehines53@gmail.com>
2022-10-06 15:09:08 -0400

    Stream code that doesnt work

---
+++ src/HW5/exercise66.java
@@ -0,0 +1,13 @@
+//package HW5;
+//
+//public class exercise66 {
+//
+//    public static boolean canItSpell(String word, String tiles) {
+//
+//        return canItSpell;
+//    }
+//
+//
+//
+//} //word = word.substring(0, word.indexOf(word_letter)) + word.substring(word.index(word_letter) + 1)
+
---
+++ src/Reveiw/Exam1Study.java
@@ -0,0 +1,17 @@
+package Reveiw;
+
+public class Exam1Study{ // opening bracket for class
+    public static double f2c(double f) {
+        return 5/9.0 * (32 - f);
+    }
+
+    public static void main(String [] args) {
+        String s = "hippopotomonstrosesquippedaliophobia";
+        System.out.println(s.charAt(0));
+        System.out.println(s.indexOf('q'));
+        System.out.println(s.indexOf('o'));
+        System.out.println(s.substring(s.indexOf('m'), 16));
+
+    }
+} // closing bracket for class
+
