bd50291c308c1c26b526e351650288ae1eddd389
cahine22 <carolinehines53@gmail.com>
2022-10-07 12:59:22 -0400

    Homework 6

--- src/HW5/exercise66.java
+++ src/HW5/exercise66.java
@@ -1,13 +1,36 @@
-//package HW5;
-//
-//public class exercise66 {
-//
-//    public static boolean canItSpell(String word, String tiles) {
-//
-//        return canItSpell;
-//    }
-//
-//
-//
-//} //word = word.substring(0, word.indexOf(word_letter)) + word.substring(word.index(word_letter) + 1)
+package HW5;
 
+public class exercise66 {
+
+    public static boolean canSpell(String word, String tiles) {
+        boolean CantSpell = false;
+        for (int i = 0; i < tiles.length(); i++) {
+            char tiles_letter = tiles.charAt(i);
+            for (int j = 0; j < word.length(); j++) {
+                char word_letter = word.charAt(j);
+                if (word_letter == tiles_letter) {
+                    word = word.substring(0, word.indexOf(word_letter))
+                            + word.substring(word.indexOf(word_letter) + 1);
+                }
+            }
+        }
+        if(word.length() == 0){
+                return true;}
+
+        return CantSpell;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canSpell("boot", "axobasrto"));
+        System.out.println(!canSpell("hippo", "haxobapsrito"));
+        System.out.println(canSpell("caroline", "jhicjoreajfwoplden"));
+        System.out.println(canSpell("computerscience", "jkkfwocmuesineoptrcece"));
+        System.out.println(!canSpell("america", "jfrwpgvkmsmjgiedca"));
+
+
+
+
+        // Student: add more test cases
+    }
+}
